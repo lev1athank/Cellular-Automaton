468b6c2fef83643e1e6429f0ae5b750721da95e7
lev1athank <nik.parh.googl@gmail.com>
2024-03-30 20:16:07 +0500

    min update

--- App/Components/Screen/Home/Sections/panels/SettingPanel/SizePanel/SizePanel.tsx
+++ App/Components/Screen/Home/Sections/panels/SettingPanel/SizePanel/SizePanel.tsx
@@ -1,13 +1,17 @@
-import React, { ChangeEvent } from 'react';
+import { ChangeEvent } from 'react';
 import style from './style.module.scss'
 const SizePanel = () => {
 
-    const setValue = (el:ChangeEvent) => {
-        const targetEl:HTMLInputElement = el.target as HTMLInputElement
+    const setValue = (el: ChangeEvent) => {
+        const targetEl: HTMLInputElement = el.target as HTMLInputElement
         console.log(targetEl.value.split('').slice(0, 2));
-        
-        if(targetEl.value.length > 2)
+
+        const value = targetEl.value
+        if (value.length > 2)
             targetEl.value = targetEl.value.split('').slice(0, 2).join('')
+        else if (+value > 30) {
+            targetEl.value = "30"
+        }
 
     }
 
@@ -21,7 +25,7 @@ const SizePanel = () => {
                     <i className={"fa-solid fa-arrows-left-right " + style.cellArrowWidth}></i>
 
                     <div className={style.inputField}>
-                        <input type="number" className={style.inputCell} defaultValue={15} onChange={setValue}/>
+                        <input type="number" className={style.inputCell} defaultValue={15} onChange={setValue} />
                         <span>px</span>
                     </div>
                 </div>
@@ -29,9 +33,16 @@ const SizePanel = () => {
             </div>
             <div className={style.FieldItem}>
                 <span className={style.title}>поле</span>
-                <div className={style.field}></div>
+                <div className={style.field}>
+                    <i className={"fa-solid fa-arrow-up " + style.fieldUpHeight + " " + style.arrow}></i>
+                    <i className={"fa-solid fa-arrow-down "  + style.fieldDownHeight + " " + style.arrow}></i>
+                    <i className={"fa-solid fa-arrow-left " + style.fieldLeftWidth + " " + style.arrow}></i>
+                    <i className={"fa-solid fa-arrow-right " + style.fieldRightWidth + " " + style.arrow}></i>
+                {/* <i className={"fa-solid fa-arrows-left-right " + style.fieldArrowWidth}></i> */}
+
             </div>
         </div>
+        </div >
     )
 }
 
